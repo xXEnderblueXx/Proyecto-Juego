b81f744953d53a055bbbb998092042d619529af4
Nikki <jesusabg36@gmail.com>
2026-02-01 20:47:02 -0400

    jodase angel
    si

--- scenes/characters/player.gd
+++ scenes/characters/player.gd
@@ -1,33 +1,55 @@
 extends PlayerCharacter
 
-@onready var animationplayer=$AnimationPlayer
-@onready var sprite2D=$Sprite2D
+@onready var anim_tree = $AnimationTree
+@onready var state_machine = anim_tree.get("parameters/playback")
 
-func _process(delta: float) -> void:
-	
-	var directionX = Input.get_axis("ui_left","ui_right")
-	var directionY = Input.get_axis("ui_up","ui_down")
-	
-	move_and_slide()
+func _ready() -> void:
+	anim_tree.active = true
 
-	animations(directionX,directionY)
-	
-	if directionX ==1:
-		sprite2D.flip_h=false
-	elif directionX ==-1:
-		sprite2D.flip_h=true
-		
-	
-		
-func animations (directionX, directionY):
-	
-	if directionX==0 and directionY==0:
-		animationplayer.play("idle_front")
-	if directionX ==1:
-		animationplayer.play("walk_x")
-	elif directionX ==-1:
-		animationplayer.play("walk_x")		
-	if directionY ==1:
-		animationplayer.play("walk_down")
-	elif directionY ==-1:
-		animationplayer.play("walk_up")
+func _process(_delta: float) -> void:
+	var current_velocity = velocity 
+
+	if current_velocity.length() == 0:
+		state_machine.travel("idle")
+
+		var last_dir = get_last_direction()
+		anim_tree.set("parameters/Idle/blend_position", last_dir)
+
+	else:
+		state_machine.travel("walk")
+
+		var move_dir = current_velocity.normalized()
+		anim_tree.set("parameters/walk/blend_position", move_dir)
+		anim_tree.set("parameters/idle/blend_position", move_dir)
+
+#extends PlayerCharacter
+#
+#@onready var animationplayer=$AnimationPlayer
+#@onready var sprite2D=$Sprite2D
+#
+#func _process(_delta: float) -> void:
+	#
+	#var directionX = Input.get_axis("ui_left","ui_right")
+	#var directionY = Input.get_axis("ui_up","ui_down")
+	#
+	#move_and_slide()
+#
+	#animations(directionX,directionY)
+	#
+	#if directionX ==1:
+		#sprite2D.flip_h=false
+	#elif directionX ==-1:
+		#sprite2D.flip_h=true
+#
+#func animations (directionX, directionY):
+	#
+	#if directionX==0 and directionY==0:
+		#animationplayer.play("idle_front")
+	#if directionX ==1:
+		#animationplayer.play("walk_x")
+	#elif directionX ==-1:
+		#animationplayer.play("walk_x")		
+	#if directionY ==1:
+		#animationplayer.play("walk_down")
+	#elif directionY ==-1:
+		#animationplayer.play("walk_up")
---
+++ scenes/levels/area_2d.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+@export var next_map: String
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "PlayerCharacter":
+		get_tree().change_scene_to_file(next_map)
